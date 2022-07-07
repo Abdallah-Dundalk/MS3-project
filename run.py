7c5de6a845465784278bdfd71dc2b18bc75e788c
Abdallah-Dundalk <Abdallah.A.Ahmed@outlook.com>
2022-07-07 13:36:21 +0000

    Initial commit - Add function to welcome user and prompt user to input column and row numbers and print message with numbers chosen.

--- run.py
+++ run.py
@@ -1,3 +1,41 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def intro_msg():
+    """
+    Prints welcome message to terminal
+    """
+    print("Lets Play Battle Ship\n")
+
+
+def user_input_col():
+    """
+    Prompts user to enter column number
+    """
+    user_col_num = input("Enter column number\n")
+    return user_col_num
+
+
+def user_input_row():
+    """
+    Prompts user to enter row number
+    """
+    user_row_num = input("Enter row number\n")
+    return user_row_num
+
+
+def print_user_input(col, row):
+    """
+    Prints message informing the user of the coloumn and row numbers chosen
+    """
+    print(f'You have chosen col {col} and row {row}')
+
+
+def main():
+    """
+    Runs all functions
+    """
+    intro_msg()
+    col_num = user_input_col()
+    row_num = user_input_row()
+    print_user_input(col_num, row_num)
+
+
+main()
