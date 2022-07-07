fa19cd232fe6c288c4c9b880072908fd1535f947
Abdallah-Dundalk <Abdallah.A.Ahmed@outlook.com>
2022-07-07 15:10:49 +0000

    Add function to append numbers input by player tp player_guess list and print message if value matches value in compare_input function

--- run.py
+++ run.py
@@ -7,25 +7,32 @@ def intro_msg():
 
 def user_input_col():
     """
-    Prompts user to enter column number
+    Prompts user to enter column number and returns an integer
     """
-    user_col_num = input("Enter column number\n")
+    user_col_num = int(input("Enter column number\n"))
     return user_col_num
 
 
 def user_input_row():
     """
-    Prompts user to enter row number
+    Prompts user to enter row number and returns an integer.
     """
-    user_row_num = input("Enter row number\n")
+    user_row_num = int(input("Enter row number\n"))
     return user_row_num
 
 
 def print_user_input(col, row):
     """
-    Prints message informing the user of the coloumn and row numbers chosen
+    Prints message informing the user of the coloumn and row numbers chosen.
     """
-    print(f'You have chosen col {col} and row {row}')
+    print(f'You have chosen column {col} and row {row}')
+
+
+def compare_input(input_value):
+    if input_value == [1, 2]:
+        print("Direct hit!!!")
+    else:
+        print("You missed")
 
 
 def main():
@@ -35,7 +42,11 @@ def main():
     intro_msg()
     col_num = user_input_col()
     row_num = user_input_row()
+    player_guess = []
+    player_guess.append(col_num)
+    player_guess.append(row_num)
     print_user_input(col_num, row_num)
+    compare_input(player_guess)
 
 
 main()
