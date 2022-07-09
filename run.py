a9cf5ec1437c7476e656a14ee4d7f6a32a243629
Abdallah-Dundalk <Abdallah.A.Ahmed@outlook.com>
2022-07-09 13:35:09 +0000

    Add function to record players guesses and function to update game board after each player guess.

--- run.py
+++ run.py
@@ -1,5 +1,18 @@
 import random
 
+computer_board_list = [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "@", "@", "@", "@"]
+
+list_1 = []
+list_2 = []
+list_3 = []
+list_4 = []
+list_5 = []
+
+
+def list_shuffler():
+    random.shuffle(computer_board_list)
+    return computer_board_list
+
 
 def intro_msg():
     """
@@ -28,7 +41,7 @@ def print_user_input(col, row):
     """
     Prints message informing the user of the coloumn and row numbers chosen.
     """
-    print(f'You have chosen column {col} and row {row}')
+    print(f'You have chosen row {row} and column {col}')
 
 
 def random_ship_gen():
@@ -56,7 +69,7 @@ def compare_input(input_value, rand_value):
         return False
 
 
-def player_guess_compare(value, returned_list):
+def player_guess_compare(rand_value):
     """
     Gets input values from user and appends to list before comparing with
     randomly generated values.If the value of the lists match, a message is
@@ -65,14 +78,15 @@ def player_guess_compare(value, returned_list):
      user that they 'missed' and the loop continues.
     """
     while True:
-        col_num = user_input_col()
         row_num = user_input_row()
+        col_num = user_input_col()
         player_guess = []
-        player_guess.append(col_num)
         player_guess.append(row_num)
+        player_guess.append(col_num)
         print_user_input(col_num, row_num)
-        shuffled_list(returned_list)
-        if compare_input(player_guess, value):
+        print_updated_computer_board(row_num, col_num)
+        memory(row_num, col_num)
+        if compare_input(player_guess, rand_value):
             break
 
 
@@ -83,22 +97,37 @@ def end():
     print("game over")
 
 
-def list_shuffler():
-    computer_board_list = [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "@", "@", "@", "@"]
-    random.shuffle(computer_board_list)
-    return computer_board_list
+def print_updated_computer_board(row_num, col_num):
+    """
+    Prints updated computer player board.
+    """
+
+    row_number = row_num
+    col_number = col_num
+
+    if row_number == 0:
+        list_1[col_number] = "x"
+    elif row_number == 1:
+        list_2[col_number] = "x"
+    elif row_number == 2:
+        list_3[col_number] = "x"
+    elif row_number == 3:
+        list_4[col_number] = "x"
+    elif row_number == 4:
+        list_5[col_number] = "x"
+    print('  '.join(list_1))
+    print('  '.join(list_2))
+    print('  '.join(list_3))
+    print('  '.join(list_4))
+    print('  '.join(list_5))
 
 
 def shuffled_list(returned_list):
     """
-    Shuffles list before slicing and printign 5 lists to represent
+    Slices shuffled list before printing 5 lists to represent
     computers player board.
     """
-    list_1 = []
-    list_2 = []
-    list_3 = []
-    list_4 = []
-    list_5 = []
+    
     slice_1 = returned_list[0:5]
     list_1.extend(slice_1)
     slice_2 = returned_list[5:10]
@@ -116,6 +145,51 @@ def shuffled_list(returned_list):
     print('  '.join(list_5))
 
 
+# def shuffled_list(returned_list, row_num, col_num):
+#     """
+#     Slices shuffled list before printing 5 lists to represent
+#     computers player board.
+#     """
+    
+#     slice_1 = returned_list[0:5]
+#     list_1.extend(slice_1)
+#     slice_2 = returned_list[5:10]
+#     list_2.extend(slice_2)
+#     slice_3 = returned_list[10:15]
+#     list_3.extend(slice_3)
+#     slice_4 = returned_list[15:20]
+#     list_4.extend(slice_4)
+#     slice_5 = returned_list[20:25]
+#     list_5.extend(slice_5)
+
+#     row_number = row_num
+#     col_number = col_num
+
+#     if row_number == 0:
+#         list_1[col_number] = "x"
+#     elif row_number == 1:
+#         list_2[col_number] = "x"
+#     elif row_number == 2:
+#         list_3[col_number] = "x"
+#     elif row_number == 3:
+#         list_4[col_number] = "x"
+#     elif row_number == 4:
+#         list_5[col_number] = "x"
+#     print('  '.join(list_1))
+#     print('  '.join(list_2))
+#     print('  '.join(list_3))
+#     print('  '.join(list_4))
+#     print('  '.join(list_5))
+
+
+storage = []
+
+
+def memory(row_num, col_num):
+    storage.append([row_num, col_num])
+    print(f'The latest value for memory test is {storage}')
+
+
 def main():
     """
     Runs all functions
@@ -124,7 +198,7 @@ def main():
     returned_list = list_shuffler()
     shuffled_list(returned_list)
     rand_value = random_ship_gen()
-    player_guess_compare(rand_value, returned_list)
+    player_guess_compare(rand_value)
     end()
 
 
